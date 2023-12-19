aac451521fa25fe2df9175839367eeb72b3ebbc9
Derek Tran <tran.derek.d@gmail.com>
2023-12-19 11:22:37 -0800

    Refactor event handlers

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import { Client, Collection, Events, GatewayIntentBits } from 'discord.js';
 import { config } from 'dotenv';
+import fs from 'fs';
 
 config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildVoiceStates] })
 
-// Executes when the Discord bot is connected
-function ready() {
-  console.log(`Ready! Logged in as ${client.user.tag}`);
+// 
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+for (const file of eventFiles) {
+	const event = await import(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(...args));
+	}
 }
-
-// Event listeneder for client ready
-client.once(Events.ClientReady, ready);
 
 // Login into Discord with client's token
 client.login(process.env.TOKEN);
