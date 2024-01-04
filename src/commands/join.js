175b57d58a8fac938cb2d9768c62009b09e04746
Derek Tran <tran.derek.d@gmail.com>
2024-01-04 13:48:58 -0800

    Add time limit for bot connection

--- src/commands/join.js
+++ src/commands/join.js
@@ -1,27 +1,50 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
-import { joinVoiceChannel } from '@discordjs/voice';
+import { entersState, joinVoiceChannel, VoiceConnectionStatus } from '@discordjs/voice';
 
 export const data = new SlashCommandBuilder()
   .setName('join')
   .setDescription('Joins your voice channel.');
 
 export async function execute(interaction) {
+  await interaction.deferReply({ephemeral: true});
+
+  let connection;
   const channel = interaction.member.voice.channel;
   const embed = new EmbedBuilder();
+
+  // Attempt to establish a connection to the user's voice channel
   if (channel) {
-    joinVoiceChannel({
+    connection = joinVoiceChannel({
       channelId: channel.id,
       guildId: channel.guild.id,
       selfDeaf: false,
       selfMute: true,
       adapterCreator: interaction.guild.voiceAdapterCreator,
     });
-    embed.setColor(0x22C55E)
-      .setTitle('Joined!');
   } else {
     embed.setColor(0xEF4444)
       .setTitle('Please join a voice channel!')
       .setDescription('You must be in a channel for the bot to join.');
+      await interaction.followUp({ embeds: [embed] });
+      return;
   }
-  await interaction.reply({ embeds: [embed], ephemeral: true });
+
+  /*
+   * Handles when a connection is not yet in the Ready state. Allows the
+   * connection 10 seconds to enter the ready state before destroying the connection
+   * and throwing an error.
+   */
+  try {
+    entersState(connection, VoiceConnectionStatus.Ready, 10e3);
+    embed.setColor(0x22C55E)
+      .setTitle('Joined!');
+    await interaction.followUp({ embeds: [embed] });
+    return connection;
+  } catch (error) {
+    connection.destroy();
+    embed.setColor(0xEF4444)
+      .setTitle('Failed to join the channel within 10 seconds!')
+    await interaction.followUp({ embeds: [embed] });
+    throw error;
+  }
 }
