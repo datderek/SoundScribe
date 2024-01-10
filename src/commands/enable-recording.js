7a8ea36760b7ed3cc4517327c635db172bddf25a
Derek Tran <tran.derek.d@gmail.com>
2024-01-10 12:56:37 -0800

    Fix so recording can be enabled without bot in channel

--- src/commands/enable-recording.js
+++ src/commands/enable-recording.js
@@ -1,30 +1,22 @@
 import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
-import { getVoiceConnection } from '@discordjs/voice';
 
 export const data = new SlashCommandBuilder()
   .setName('enable-recording')
-  .setDescription('Enables recording for the current user!')
+  .setDescription('Enables recording for the invoked user.')
 
 export async function execute(interaction) {
   const embed = new EmbedBuilder();
-  const connection = getVoiceConnection(interaction.guildId);
   
   /*
    * Adds the user to the set of recordable users.
    */
-  if (connection) {
-    const userName = interaction.member.user.username;
-    const userId = interaction.member.user.id;
-    interaction.client.recordable.add(userId);
+  const userName = interaction.member.user.username;
+  const userId = interaction.member.user.id;
+  interaction.client.recordable.add(userId);
 
-    embed.setColor(0x22C55E)
-      .setTitle('Enabled recording!')
-      .setDescription(`The bot will now record audio from ${userName}`);
-  } else {
-    embed.setColor(0xEF4444)
-      .setTitle('SoundScribe is not ready!')
-      .setDescription('Please use `/join` to invite the bot to your channel then try again.')
-  }
+  embed.setColor(0x22C55E)
+    .setTitle('Recording enabled.')
+    .setDescription(`The bot will now record audio from ${userName}`);
 
   return await interaction.reply({ embeds: [embed], ephemeral: true });
 }
