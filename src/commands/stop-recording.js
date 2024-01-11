707ed7dfdbdce1b1cd12ee7a3f40e0a58077ea23
Derek Tran <tran.derek.d@gmail.com>
2024-01-11 15:11:36 -0800

    Transcribe audio on recording finish

--- src/commands/stop-recording.js
+++ src/commands/stop-recording.js
@@ -20,11 +20,35 @@ export async function execute (interaction) {
     for (const [ /*userId*/, stream ] of receiver.subscriptions) {
       stream.emit('end');
     }
+
+    /*
+     * Create Python child processes to transcribe the audio for each user and 
+     * output the transcription in Discord embeds.
+     */
+    let transcription = '';
+    for (const fileName of interaction.client.recordingFileNames) {
+      const childPython = spawn('python', ['./src/utils/transcribe.py', 
+        `${fileName}`]);
+      childPython.stdout.on('data', async (data) => {
+        const exampleEmbed = new EmbedBuilder()
+	        .setTitle('Transcription completed.')
+	        .setDescription(`${data}`);
+        await interaction.editReply({embeds: [exampleEmbed]});
+      })
+      childPython.stderr.on('data', (data) => {
+        console.error(`stderr: ${data}`);
+      })
+      childPython.on('close', (code) => {
+      })
+    }
+
     connection.destroy();
     interaction.client.recordingFileNames = [];
+    interaction.client.recordable.clear();
     
     embed.setColor(0x22C55E)
-      .setTitle("Recording has successfully ended.");
+      .setTitle("Recording has successfully ended.")
+      .setDescription("Please wait while the audio is being transcribed...")
   } else {
     embed.setColor(0xEF4444)
       .setTitle("Bot is not currently recording.");
--- src/utils/transcribe.py
+++ src/utils/transcribe.py
@@ -1,7 +1,7 @@
 import whisper
 import sys
 
-fileName = sys.argv[1]
+fileName = str(sys.argv[1])
 model = whisper.load_model("tiny.en")
-result = model.transcribe(f"./recordings/{fileName}", fp16=False)
+result = model.transcribe(f"{fileName}", fp16=False)
 print(result['text'])
