a86b3573bd2987505a64436228f2bf0c011c6d7b
Derek Tran <tran.derek.d@gmail.com>
2024-01-19 10:43:32 -0800

    WIP: Real time transcription child process

--- src/utils/transcribe.py
+++ src/utils/transcribe.py
@@ -1,6 +1,32 @@
-import sys;
+import whisper
+import sys
+from datetime import datetime, timedelta
+from queue import Queue
 
-# Read in data from stdin
-for data in sys.stdin:
-  print(f"Received data from Node.js: {data.strip()}")
+model = whisper.load_model('tiny.en')
+audio_data_queue = Queue()
+last_phrase_time = None
 
+while True:
+  # Saves new audio data into a queue
+  for line in sys.stdin:
+    audio_data_queue.put(line)
+
+  now = datetime.utcnow()
+
+  if not audio_data_queue.empty():
+    phrase_complete = False
+    if last_phrase_time and now - last_phrase_time > timedelta(seconds=5):
+        phrase_complete = True
+        print("Reached it here")
+    
+    last_phrase_time = now
+
+    audio_data = ''.join(audio_data_queue.queue)
+    audio_data_queue.queue.clear()
+
+    result = model.transcribe(audio_data, fp16=False)
+    text = result['text'].strip()
+    print(text)
+    
+    
