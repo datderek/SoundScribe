cc383337bf1f4c352f844f67dc02fb223c08a78f
Derek Tran <tran.derek.d@gmail.com>
2024-01-23 19:19:45 -0800

    Add comments for clarity

--- src/utils/transcribe.py
+++ src/utils/transcribe.py
@@ -6,31 +6,33 @@ from queue import Queue
 from time import sleep
 
 print("Loading transcription model", flush = True)
-model = whisper.load_model("base.en")
+model = whisper.load_model("small.en")
 print("Model has loaded.", flush = True)
 data_queue = Queue()
 
-# Background thread to constantly receive input data from stdin
-# so that the main thread is not blocked
+# Background thread set up to receive input data from stdin so that the 
+# main thread is not blocked
 def read_input():
   while True:
     data_chunk = sys.stdin.buffer.read(1024)
     if data_chunk:
       data_queue.put(data_chunk)
       data_chunk = None
-
 input_thread = threading.Thread(target=read_input)
 input_thread.start()
 
+# Main thread handles the transcription when data is present within the queue
 while True:
   if not data_queue.empty():
     data = b''.join(data_queue.queue)
     data_queue.queue.clear()
 
+    # Format the PCM audio into NumPy array containing the audio as a float32
+    # monowave form
     np_data = np.frombuffer(data, dtype=np.int16).astype(np.float32) / 32768.0
-
+    
     result = model.transcribe(np_data, fp16 = False)
-    text = result['text']
-    print(f"Transcription: {text}", flush = True)
-    sleep(1)
+    text = result['text'].strip()
+    print(f"{text}", flush = True)
+    sleep(0.25)
     
